e94c997824492fcc331dfee03f218b26a5da4213
MoonGoon <glass23000@naver.com>
2024-11-05 15:43:16 +0900

    feat: Retrospect Entity 생성 및 CoreDataClass 생성
    Co-authored-by: alstjr7437 <alstjr7437@naver.com>

---
+++ RetsTalk/RetsTalk/Data/Repository/Retrospect+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Retrospect+CoreDataClass.swift
+//  RetsTalk
+//
+//  Created by MoonGoon on 11/5/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Retrospect)
+public class Retrospect: NSManagedObject {
+
+}
