3102967fc9121d6f8f5325662c7b086322c6ba9b
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-05 15:30:07 +0900

    feat: Message Entity 생성 및 CoreDataClass 생성

---
+++ RetsTalk/RetsTalk/Data/Repository/Message+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  Message+CoreDataClass.swift
+//  RetsTalk
+//
+//  Created by KimMinSeok on 11/5/24.
+//
+//
+
+import Foundation
+import CoreData
+
+public class Message: NSManagedObject {
+
+}
