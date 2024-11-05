578c53768692bc5265bdb51e179e65016609c2d1
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-05 18:35:47 +0900

    feat: CoreDataStorage 싱글톤으로 변경

---
+++ RetsTalk/RetsTalk/Data/CoreDataStorage.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataStorage.swift
+//  RetsTalk
+//
+//  Created by KimMinSeok on 11/5/24.
+//
+
+import CoreData
+
+final class CoreDataStorage {
+    
+    static let shared = CoreDataStorage()
+    
+    var context : NSManagedObjectContext { persistentContainer.viewContext }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "RetsTalk")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    private init() { }
+    
+    // MARK: - Core Data Saving support
+    func saveContext() throws {
+        if context.hasChanges {
+            try context.save()
+        }
+    }
+    
+}
+
