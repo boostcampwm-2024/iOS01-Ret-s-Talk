86219d8bfc612b94c1c7c0d4bd315e3d143cbe7d
Koo <31850425+gen-com@users.noreply.github.com>
2024-11-05 18:14:06 +0900

    test: 네트워크 요청 합성 테스트 및 목 서버 초안 추가

---
+++ RetsTalk/NetworkTests/MockServerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MockServerTests.swift
+//  NetworkTests
+//
+//  Created by Byeongjo Koo on 11/5/24.
+//
+
+import XCTest
+
+final class MockServerTests: XCTestCase {
+    private var fetcher: NetworkRequestable?
+
+    func test_서버_요청에서_URLRequest가_기대값처럼_형성되었는지() async throws {
+        
+    }
+}
