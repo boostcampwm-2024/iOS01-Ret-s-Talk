101b6e469f5a7afae39aa06290fc842052a6d42f
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-05 18:52:27 +0900

    feat: Message에 Save 기능 구현
    Co-Authored-By: MoonGoon <moongoon.cnu@gmail.com>

--- RetsTalk/RetsTalk/Data/Repository/Message+CoreDataClass.swift
+++ RetsTalk/RetsTalk/Data/Repository/Message+CoreDataClass.swift
@@ -10,5 +10,20 @@ import Foundation
 import CoreData
 
 public class Message: NSManagedObject {
-
+    
+    @discardableResult
+    static func addAndSave(with object: MessageDTO) -> Result<Message, Error> {
+        do {
+            let context = CoreDataStorage.shared.context
+            let message = Message(context: context)
+            message.content = object.content
+            message.isUser = object.isUser
+            
+            try CoreDataStorage.shared.saveContext()
+            return .success(message)
+        } catch {
+            return .failure(error)
+        }
+        
+    }
 }
