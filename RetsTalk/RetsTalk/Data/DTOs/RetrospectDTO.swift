df4dd87f02c2c1c655a4945caf09d07b6035dc97
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-06 21:03:42 +0900

    feat: 회고 DTO Entity Mapping 해주는 함수 구현
    Co-Authored-By: MoonGoon <moongoon.cnu@gmail.com>

--- RetsTalk/RetsTalk/Data/DTOs/RetrospectDTO.swift
+++ RetsTalk/RetsTalk/Data/DTOs/RetrospectDTO.swift
@@ -6,11 +6,27 @@
 //
 
 import Foundation
+import CoreData
 
-struct RetrospectDTO {
+struct RetrospectDTO: CoreDataConvertible {
+    typealias Entity = RetrospectEntity
+    
     let summary: String?
     let isFinished: Bool
     let isBookmarked: Bool
     let createdAt: Date
     let chat: [MessageDTO]
+    
+    func toEntity(context: NSManagedObjectContext) -> RetrospectEntity {
+        let retrospect = RetrospectEntity(context: context)
+        retrospect.summary = summary
+        retrospect.isFinished = isFinished
+        retrospect.isBookmarked = isBookmarked
+        retrospect.createdAt = createdAt
+        retrospect.chat = NSSet(array: chat.map {
+            $0.toEntity(context: context)
+        })
+        
+        return retrospect
+    }
 }
