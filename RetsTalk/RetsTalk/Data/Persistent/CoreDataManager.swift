f365e0953df1f0bc28595630870018927fe443e2
MoonGoon <glass23000@naver.com>
2024-11-06 17:56:44 +0900

    feat: CoreData 간편 저장을 위한 Manager 추가
    Co-authored-by: alstjr7437 <alstjr7437@naver.com>

--- RetsTalk/RetsTalk/Data/Persistent/CoreDataManager.swift
+++ RetsTalk/RetsTalk/Data/Persistent/CoreDataManager.swift
@@ -5,4 +5,13 @@
 //  Created by MoonGoon on 11/6/24.
 //
 
-import Foundation
+enum CoreDataManager {
+    @discardableResult
+    static func addAndSave<T: CoreDataConvertible>(with object: T) throws -> T.Entity {
+        let coreDataStorage = CoreDataStorage.shared
+        let entity = object.toEntity(context: coreDataStorage.context)
+        try coreDataStorage.saveContext()
+
+        return entity
+    }
+}
