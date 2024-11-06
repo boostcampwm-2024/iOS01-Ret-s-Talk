b5d1f715eb55597be76083d08e4c9917d9917f4c
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-06 18:02:34 +0900

    refactor: 이전 Entity에서 Save를 직접 해주는 코드 변경
    Co-Authored-By: MoonGoon <moongoon.cnu@gmail.com>

--- RetsTalk/RetsTalk/Data/Persistent/MessageEntity+CoreDataClass.swift
+++ RetsTalk/RetsTalk/Data/Persistent/MessageEntity+CoreDataClass.swift
@@ -8,19 +8,4 @@
 import Foundation
 import CoreData
 
-public class MessageEntity: NSManagedObject {
-    @discardableResult
-    static func addAndSave(with object: MessageDTO) -> Result<MessageEntity, Error> {
-        do {
-            let context = CoreDataStorage.shared.context
-            let message = MessageEntity(context: context)
-            message.content = object.content
-            message.isUser = object.isUser
-
-            try CoreDataStorage.shared.saveContext()
-            return .success(message)
-        } catch {
-            return .failure(error)
-        }
-    }
-}
+public class MessageEntity: NSManagedObject { }
--- RetsTalk/RetsTalk/Data/Persistent/RetrospectEntity+CoreDataClass.swift
+++ RetsTalk/RetsTalk/Data/Persistent/RetrospectEntity+CoreDataClass.swift
@@ -9,5 +9,4 @@
 import Foundation
 import CoreData
 
-@objc(RetrospectEntity)
 public class RetrospectEntity: NSManagedObject { }
