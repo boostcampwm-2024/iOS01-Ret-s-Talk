2d821057717a558561178ed732289a4375d1fb01
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-07 14:30:54 +0900

    feat: Message Store에 FetchAll과 RemoveAll 기능 구현
    Co-Authored-By: MoonGoon <moongoon.cnu@gmail.com>

--- RetsTalk/RetsTalk/Data/Persistent/CoreDataMessageStorage.swift
+++ RetsTalk/RetsTalk/Data/Persistent/CoreDataMessageStorage.swift
@@ -14,10 +14,26 @@ final class CoreDataMessageStorage {
         self.coreDataStorage = coreDataStorage
     }
 
+    @discardableResult
     func save(_ message: Message) throws -> Message {
         let entity = MessageEntity(from: message, insertInfo: coreDataStorage.context)
         try coreDataStorage.saveContext()
 
         return entity.toDomain()
+    }
+    
+    func fetchAll() throws -> [Message] {
+        let fetchRequest = try coreDataStorage.context.fetch(MessageEntity.fetchRequest())
+        let messages = fetchRequest.map { $0.toDomain() }
+        
+        return messages
+    }
+    
+    func removeAll() throws {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = MessageEntity.fetchRequest()
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+
+        try coreDataStorage.context.execute(deleteRequest)
+        try coreDataStorage.saveContext()
     }
 }
