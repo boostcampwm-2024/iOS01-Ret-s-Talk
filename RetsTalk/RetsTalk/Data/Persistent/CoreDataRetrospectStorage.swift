572cecea46909702589ef655da25112e11fb2bf7
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-07 14:45:09 +0900

    feat: 회고 Storage에 FetchAll과 RemoveAll 구현
    Co-Authored-By: MoonGoon <moongoon.cnu@gmail.com>

--- RetsTalk/RetsTalk/Data/Persistent/CoreDataRetrospectStorage.swift
+++ RetsTalk/RetsTalk/Data/Persistent/CoreDataRetrospectStorage.swift
@@ -14,10 +14,26 @@ final class CoreDataRetrospectStorage {
         self.coreDataStorage = coreDataStorage
     }
 
+    @discardableResult
     func save(_ retrospect: Retrospect) throws -> Retrospect {
-        let entity = RetrospectEntity()
+        let entity = RetrospectEntity(from: retrospect, insertInfo: coreDataStorage.context)
         try coreDataStorage.saveContext()
 
         return try entity.toDomain()
+    }
+    
+    func fetchAll() throws -> [Retrospect] {
+        let fetchRequest = try coreDataStorage.context.fetch(RetrospectEntity.fetchRequest())
+        let retrospects = try fetchRequest.map { try $0.toDomain() }
+        
+        return retrospects
+    }
+    
+    func removeAll() throws {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = RetrospectEntity.fetchRequest()
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+
+        try coreDataStorage.context.execute(deleteRequest)
+        try coreDataStorage.saveContext()
     }
 }
