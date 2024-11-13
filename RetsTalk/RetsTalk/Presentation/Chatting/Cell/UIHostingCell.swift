075613b380eb6b302bb23beaee362f12c699ec10
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-13 11:09:54 +0900

    feat: 채팅 UI구현을 위한 파일 구성
    - 유저, Assistant Cell
    - SwiftUI Hosting을 위한 class
    - 입력창 View
    - 채팅 ViewController
    
    Co-Authored-By: Hanseung Cho <110288835+1win2@users.noreply.github.com>

---
+++ RetsTalk/RetsTalk/Presentation/Chatting/Cell/UIHostingCell.swift
@@ -0,0 +1,13 @@
+//
+//  UIHostingCell.swift
+//  RetsTalk
+//
+//  Created by KimMinSeok on 11/13/24.
+//
+
+
+import SwiftUI
+
+final class UIHostingCell {
+    
+}
---
+++ RetsTalk/RetsTalk/Presentation/Chatting/ChattingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChattingViewController.swift
+//  RetsTalk
+//
+//  Created by KimMinSeok on 11/13/24.
+//
+
+import UIKit
+
+final class ChattingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
