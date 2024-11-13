1019f23cb5109184749132d0eeb089a2be1ae663
Hanseung Cho <1win2@naver.com>
2024-11-13 15:47:04 +0900

    style: 컬러셋, 폰트셋 열거형 작성

--- RetsTalk/RetsTalk/Utility/Enum/ColorSet.swift
+++ RetsTalk/RetsTalk/Utility/Enum/ColorSet.swift
@@ -5,3 +5,21 @@
 //  Created by HanSeung on 11/13/24.
 //
 
+enum ColorSet {
+    case blazingOrange
+    case blueberry
+    
+    case background_main
+    case background_retrospect
+    case stroke_retrospect
+    
+    var hexCode: String {
+        switch self {
+        case .blazingOrange: return "FFA44A"
+        case .blueberry: return "2C3E50"
+        case .background_main: return "FAFAFA"
+        case .background_retrospect: return "FFFFFF"
+        case .stroke_retrospect: return "F1F1F1"
+        }
+    }
+}
