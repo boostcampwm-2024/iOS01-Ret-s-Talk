c9a17a7741ebedf1b2515ba5065856cb286a326e
Koo <31850425+gen-com@users.noreply.github.com>
2024-11-19 10:49:43 +0900

    refactor: 오타 및 네이밍 수정

--- RetsTalk/RetsTalk/Persistent/EntityRepresentable.swift
+++ RetsTalk/RetsTalk/Persistent/EntityRepresentable.swift
@@ -7,9 +7,9 @@
 
 protocol EntityRepresentable: Sendable {
     /// 현재의 데이터를 기반으로 변환한 딕셔너리 값.
-    var dictionaryValue: [String: Any] { get }
+    var mappingDictionary: [String: Any] { get }
     
-    init(dictinary: [String: Any])
+    init(dictionary: [String: Any])
     
     static var entityName: String { get }
 }
--- RetsTalk/RetsTalk/Persistent/Persistable.swift
+++ RetsTalk/RetsTalk/Persistent/Persistable.swift
@@ -11,7 +11,7 @@ protocol Persistable {
     /// 로컬 저장소에 엔티티 데이터를 추가합니다.
     /// - Parameter entities: 추가할 엔티티 배열.
     /// - Returns: 추가된 데이터.
-    func add<Entity>(contentOf entities: [Entity]) async throws -> [Entity] where Entity: EntityRepresentable
+    func add<Entity>(contentsOf entities: [Entity]) async throws -> [Entity] where Entity: EntityRepresentable
     /// 로컬 저장소에서 요청 조건에 맞는 엔티티 데이터를 불러옵니다.
     /// - Parameter request: 요청 조건.
     /// - Returns: 엔티티 데이터.
@@ -29,5 +29,5 @@ protocol Persistable {
     ) async throws -> Entity where Entity: EntityRepresentable
     /// 로컬 저장소에서 엔티티 데이터를 제거합니다.
     /// - Parameter entities: 제거할 엔티티 데이터.
-    func delete<Entity>(contentOf entities: [Entity]) async throws where Entity: EntityRepresentable
+    func delete<Entity>(contentsOf entities: [Entity]) async throws where Entity: EntityRepresentable
 }
