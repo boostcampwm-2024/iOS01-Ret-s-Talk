a6b9218b8b51d62d9c0f677836bad06820dc72a2
Koo <31850425+gen-com@users.noreply.github.com>
2024-11-19 10:44:23 +0900

    refactor: 엔티티 추상 수정

--- RetsTalk/RetsTalk/Persistent/EntityRepresentable.swift
+++ RetsTalk/RetsTalk/Persistent/EntityRepresentable.swift
@@ -8,8 +8,6 @@
 protocol EntityRepresentable: Sendable {
     /// 현재의 데이터를 기반으로 변환한 딕셔너리 값.
     var dictionaryValue: [String: Any] { get }
-    /// 해당 엔티티를 식별하기 위해 필요한 속성 딕셔너리 값.
-    var matchingAttributes: [String: Any] { get }
     
     init(dictinary: [String: Any])
     
