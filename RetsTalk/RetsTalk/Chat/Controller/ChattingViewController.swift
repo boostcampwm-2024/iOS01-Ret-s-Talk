d3b18a178e92c56bf8aeaf6e0bc7f342bea69e77
MoonGoon <glass23000@naver.com>
2024-11-20 16:18:56 +0900

    refactor: 채팅 액션 기능 수정
    - combine을 사용한 반응형으로 view를 업데이트 할 수 있도록 수정

--- RetsTalk/RetsTalk/Chat/Controller/ChattingViewController.swift
+++ RetsTalk/RetsTalk/Chat/Controller/ChattingViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import SwiftUI
+import Combine
 
 final class ChattingViewController: UIViewController {
     private let chatView = ChatView()
     private let messageManager: MockMessageManager = MockMessageManager(
         messageManagerListener: MockMessageManagerListener()
     )
+    private var cancellables: Set<AnyCancellable> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +23,9 @@ final class ChattingViewController: UIViewController {
         chatView.delegate = self
 
         addKeyboardObservers()
-        messageManager.messages = messageManager.fetchMessages(offset: 0, amount: 10)
+        messageManager.fetchMessages(offset: Numeric.initialOffset, amount: Numeric.amount)
+
+        observeMessages()
     }
     
     override func loadView() {
@@ -52,6 +56,27 @@ final class ChattingViewController: UIViewController {
     @objc private func keyboardWillHide(_ notification: Notification) {
         chatView.updateBottomConstraintForKeyboard(height: 40)
     }
+
+    private func observeMessages() {
+        var previousMessageCount = messageManager.messages.count
+
+        messageManager.messagePublisher
+            .dropFirst()
+            .receive(on: RunLoop.main)
+            .print("🔵 Combine Debug") // Combine 스트림 디버그
+            .sink { [weak self] newMessages in
+                guard let self = self else { return }
+
+                let oldCount = previousMessageCount
+                let newCount = newMessages.count
+                previousMessageCount = newCount
+                guard oldCount < newCount else { return }
+
+                let newIndexPaths = (oldCount..<newCount).map { IndexPath(row: $0, section: 0) }
+                chatView.insertMessages(at: newIndexPaths)
+            }
+            .store(in: &cancellables)
+    }
 }
 
 extension ChattingViewController: UITableViewDelegate, UITableViewDataSource {
@@ -71,30 +96,29 @@ extension ChattingViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
+// MARK: - ChatViewDelegate
+
 extension ChattingViewController: ChatViewDelegate {
-    func sendMessage(with text: String) {
-        let initialMessageCount = messageManager.messages.count
+    func sendMessage(_ chatView: ChatView, with text: String) {
         let userMessage = Message(role: .user, content: text, createdAt: Date())
         // 실제로는 비동기 처리 or 반응형으로 처리가 되어야 함, 아직 미완된 기능이라 일단 넘어가도록 하였음
-        messageManager.messages.append(userMessage)
-        let userIndexPath = IndexPath(row: initialMessageCount, section: 0)
-
-        chatView.insertMessages(at: [userIndexPath])
-        chatView.scrollToBottom()
-
         Task {
             do {
-                let responseMessage = try await messageManager.send(userMessage)
-                messageManager.messages.append(responseMessage)
+                try await messageManager.send(userMessage)
 
-                let responseIndexPath = IndexPath(row: messageManager.messages.count - 1, section: 0)
-                chatView.insertMessages(at: [responseIndexPath])
-
-                chatView.scrollToBottom()
                 chatView.updateSendButtonState(isEnabled: true)
             } catch {
                 print("response error")
             }
         }
+    }
+}
+
+// MARK: - Constants
+
+private extension ChattingViewController {
+    enum Numeric {
+        static let initialOffset = 0
+        static let amount = 10
     }
 }
