93769a5047946bdb8e7209f6a4b48cbc0c0bac0f
Hanseung Cho <1win2@naver.com>
2024-11-20 20:25:06 +0900

    feat: 회고목록 화면 테이블뷰의 swiftUI 커스텀 셀 적용

--- RetsTalk/RetsTalk/Retrospect/Controller/RetrospectViewController.swift
+++ RetsTalk/RetsTalk/Retrospect/Controller/RetrospectViewController.swift
@@ -6,23 +6,86 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class RetrospectListViewController: UIViewController {
+    let retrospectListView = RetrospectListView()
+    
+    private let dataSource = [
+        ("해야할 일의 절반밖에 못했지만, 속도보다 방향이 더 중요하다는 걸 배운 날이었다.", Date()),
+        ("회고의 중요성을 깨닫고, 혼자만이 아닌 함께 성장하기 위해 남은 시간 동안 성실히 임할 것을 다짐.", Date()),
+        ("계획을 계속 수정했지만, 그 과정 속에서 나의 우선순위를 다시 생각해볼 수 있었다.", Date()),
+        ("혼자서는 막막했던 문제도 함께 고민하니 쉽게 풀리며, 협업의 힘을 실감한 하루였다.", Date()),
+        ("코드 리뷰를 통해 생각지도 못한 개선점을 알게 되었고, 내 자신을 돌아볼 수 있는 계기가 되었다.", Date()),
+        ("해야할 일의 절반밖에 못했지만, 속도보다 방향이 더 중요하다는 걸 배운 날이었다.", Date()),
+        ("회고의 중요성을 깨닫고, 혼자만이 아닌 함께 성장하기 위해 남은 시간 동안 성실히 임할 것을 다짐.", Date()),
+        ("계획을 계속 수정했지만, 그 과정 속에서 나의 우선순위를 다시 생각해볼 수 있었다.", Date()),
+        ("혼자서는 막막했던 문제도 함께 고민하니 쉽게 풀리며, 협업의 힘을 실감한 하루였다.", Date()),
+        ("코드 리뷰를 통해 생각지도 못한 개선점을 알게 되었고, 내 자신을 돌아볼 수 있는 계기가 되었다.", Date()),
+    ]
     
     // MARK: ViewController lifecycle method
-
+    
+    override func loadView() {
+        view = retrospectListView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .backgroundMain // 임시 배경색 설정
         setUpNavigationBar()
+        retrospectListView.setTableViewDelegate(self)
     }
     
     // MARK: Custom method
-
+    
     private func setUpNavigationBar() {
         title = Texts.titleLabelText
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource conformance
+
+extension RetrospectListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = dataSource[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: Texts.cellIdentifier, for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.contentConfiguration = UIHostingConfiguration {
+            RetrospectCell(summary: data.0, createdAt: data.1)
+        }
+        .margins(.vertical, 4)
+        
+        return cell
+    }
+    
+    // MARK: Section 임시 생성 코드
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "11월"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let header = view as? UITableViewHeaderFooterView {
+            header.textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+            header.textLabel?.textColor = UIColor.black
+            header.contentView.backgroundColor = .clear
+        }
     }
 }
 
@@ -30,6 +93,7 @@ final class RetrospectListViewController: UIViewController {
 
 extension RetrospectListViewController {
     enum Texts {
+        static let cellIdentifier = "RetrospectCell"
         static let titleLabelText = "회고"
     }
 }
