b6d1f628a8518b1a8f447d73aa8c2b32431ebc26
Hanseung Cho <1win2@naver.com>
2024-11-20 20:07:58 +0900

    feat: RetrospectCell 구현

--- RetsTalk/RetsTalk/Retrospect/View/Cell/RetrospectCell.swift
+++ RetsTalk/RetsTalk/Retrospect/View/Cell/RetrospectCell.swift
@@ -1,1 +1,103 @@
+import SwiftUI
 
+struct RetrospectCell: View {
+    let summary: String
+    let createdAt: Date
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            SummaryText(summary)
+            Spacer()
+                .frame(height: Metrics.padding)
+            CreatedDateText(createdAt)
+        }
+        .padding(Metrics.padding)
+        .background(Color(Texts.cellBackgroundColorName))
+        .cornerRadius(Metrics.cornerRadius)
+        .overlay(
+            RoundedRectangle(cornerRadius: Metrics.cornerRadius)
+                .stroke(Color(Texts.cellStrokeColorName), lineWidth: Metrics.RectangleStrokeWidth)
+        )
+        .frame(maxWidth: .infinity)
+    }
+}
+
+// MARK: - Subviews
+
+private extension RetrospectCell {
+    struct SummaryText: View {
+        let content: String
+        
+        init(_ content: String) {
+            self.content = content
+        }
+    
+        var body: some View {
+            HStack(alignment: .top) {
+                Text(content.charWrapping)
+                    .font(Font(UIFont.appFont(.semiTitle)))
+                    .lineLimit(Numerics.summaryTextLineLimit)
+                    .truncationMode(.tail)
+                Spacer()
+            }
+            .frame(height: 40, alignment: .topLeading)
+        }
+    }
+    
+    struct CreatedDateText: View {
+        let date: Date
+        
+        init(_ date: Date) {
+            self.date = date
+        }
+        
+        var body: some View {
+            Text(formatDateToKoreanStyle(date: date))
+                .font(Font(UIFont.appFont(.caption)))
+                .foregroundStyle(.blueBerry)
+        }
+        
+        private func formatDateToKoreanStyle(date: Date) -> String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: Texts.dateLocaleIdentifier)
+            dateFormatter.dateFormat = Texts.dateFormat
+            return dateFormatter.string(from: date)
+        }
+    }
+}
+
+// MARK: - Constants
+
+private extension RetrospectCell {
+    enum Metrics {
+        static let margin = 16.0
+        static let padding = 10.0
+        static let cornerRadius = 12.0
+        static let RectangleStrokeWidth = 1.0
+    }
+    
+    enum Numerics {
+        static let summaryTextLineLimit = 2
+    }
+    
+    enum Texts {
+        static let cellBackgroundColorName = "BackgroundRetrospect"
+        static let cellStrokeColorName = "StrokeRetrospect"
+        
+        static let dateFormat = "M월 d일 EEEE"
+        static let dateLocaleIdentifier = "ko_KR"
+    }
+}
+
+struct RetrospectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("BackgroundMain").edgesIgnoringSafeArea(.all)
+            VStack {
+                RetrospectCell(summary: "디버깅에 너무너무 지친 하루였지만, 원인을 찾고 문제를 해결하면서 더 단단해진 뭐 그런 기분이다. 최대한 길이를 길게 써보려고 함", createdAt: Date())
+                RetrospectCell(summary: "디버깅에 지친 하루이다.", createdAt: Date())
+                RetrospectCell(summary: "디버깅에 지친 하루였지만, 원인을 찾고 문제를 해결하면서 조금 더 단단해진 기분이다.", createdAt: Date())
+            }
+        }
+    }
+}
