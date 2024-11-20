36086814346d8713a19e3ec069030f0f695af16d
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-20 11:40:33 +0900

    feat: message매니저가 persistent 추상타입을 가지도록 구현

--- RetsTalk/RetsTalk/Chat/Model/MessageManager.swift
+++ RetsTalk/RetsTalk/Chat/Model/MessageManager.swift
@@ -11,10 +11,16 @@ final class MessageManager: MessageManageable {
     let retrospectID: UUID
     private(set) var messages: [Message] = []
     private(set) var messageManagerListener: MessageManagerListener
+    let persistent: Persistable
     
-    init(retrospectID: UUID, messageManagerListener: MessageManagerListener) {
+    init(
+        retrospectID: UUID,
+        messageManagerListener: MessageManagerListener,
+        persistent: Persistable
+    ) {
         self.retrospectID = retrospectID
         self.messageManagerListener = messageManagerListener
+        self.persistent = persistent
     }
     
     func fetchMessages(offset: Int, amount: Int) {
