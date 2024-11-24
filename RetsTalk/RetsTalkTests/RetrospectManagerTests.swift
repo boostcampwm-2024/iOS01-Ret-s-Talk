89c1bd6a091178f7bd72f891e9aa2058a2f649e1
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-24 17:08:26 +0900

    feat: 회고를 불러오는지 테스트 구현

---
+++ RetsTalk/RetsTalkTests/RetrospectManagerTests.swift
@@ -0,0 +1,42 @@
+//
+//  RetrospectManagerTests.swift
+//  RetsTalkTests
+//
+//  Created by KimMinSeok on 11/24/24.
+//
+
+import XCTest
+
+final class RetrospectManagerTests: XCTestCase {
+    private var retrospectManager: RetrospectManager?
+    private let sharedUserID = UUID()
+    
+    private var testableMessages: [Retrospect] = []
+    
+    override func setUp() {
+        super.setUp()
+        
+        testableMessages = [
+            Retrospect(userID: sharedUserID),
+            Retrospect(userID: sharedUserID),
+            Retrospect(userID: sharedUserID),
+            Retrospect(userID: sharedUserID),
+            Retrospect(userID: sharedUserID),
+        ]
+        
+        retrospectManager = RetrospectManager(
+            userID: UUID(),
+            retrospectStorage: MockRetrospectStore(retrospects: testableMessages),
+            assistantMessageProvider: MockAssistantMessageProvider()
+        )
+    }
+    
+    func test_회고를_불러올_수_있는가() async throws {
+        let retrospectManager = try XCTUnwrap(retrospectManager)
+        
+        try await retrospectManager.fetchRetrospects(offset: 0, amount: 2)
+        
+        let retrospectResult = retrospectManager.retrospects
+        XCTAssertEqual(retrospectResult.count, 2)
+    }
+}
