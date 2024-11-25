4718dbe63291416e2ff9f897a811e6ee0b5f14bb
Hanseung Cho <1win2@naver.com>
2024-11-25 17:53:42 +0900

    fix: 커스텀메서드 삭제 및 persistable 메서드로 병합

--- RetsTalk/RetsTalk/Persistent/Implementation/UserDefaultsManager.swift
+++ RetsTalk/RetsTalk/Persistent/Implementation/UserDefaultsManager.swift
@@ -17,37 +17,35 @@ final class UserDefaultsManager: Persistable {
     // MARK: Persistable conformance
 
     func add<Entity>(contentsOf entities: [Entity]) -> [Entity] where Entity : EntityRepresentable {
-        return []
+        let initializingEntity = Entity.init(dictionary: [:])
+        let dictionary = initializingEntity.mappingDictionary
+        
+        for dictionaryKey in dictionary.keys {
+            userDefaultsContainer.set(dictionary[dictionaryKey], forKey: dictionaryKey)
+        }
+        return [initializingEntity]
     }
     
     func fetch<Entity>(by request: any PersistFetchRequestable<Entity>) -> [Entity] where Entity : EntityRepresentable {
-        return []
+        guard let entityDictionary = userDefaultsContainer.dictionary(forKey: Entity.entityName) else {
+            return []
+        }
+        
+        return [Entity(dictionary: entityDictionary)]
     }
     
     func update<Entity>(
         from sourceEntity: Entity,
         to updatingEntity: Entity
     ) -> Entity where Entity : EntityRepresentable {
-        return Entity(dictionary: [:])
+        let dictionary = updatingEntity.mappingDictionary
+        for dictionaryKey in dictionary.keys {
+            userDefaultsContainer.set(dictionary[dictionaryKey], forKey: dictionaryKey)
+        }
+        return updatingEntity
     }
     
     func delete<Entity>(contentsOf entities: [Entity]) where Entity : EntityRepresentable {
         
-    }
-    
-    func fetch<Entity>() -> Entity? where Entity : EntityRepresentable {
-        guard let entityDictionary = userDefaultsContainer.dictionary(forKey: Entity.entityName) else {
-            return nil
-        }
-        
-        return Entity(dictionary: entityDictionary)
-    }
-    
-    func update<Entity>(entity: Entity) where Entity: EntityRepresentable {
-        let dictionary = entity.mappingDictionary
-        
-        for dictionaryKey in dictionary.keys {
-            userDefaultsContainer.set(dictionary[dictionaryKey], forKey: dictionaryKey)
-        }
     }
 }
