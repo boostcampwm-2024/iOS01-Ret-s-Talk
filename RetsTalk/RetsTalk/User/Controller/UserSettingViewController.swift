e514e31839594274e15bac82825a85f13de554ac
Hanseung Cho <1win2@naver.com>
2024-11-26 00:08:06 +0900

    refactor: 리뷰 반영 UIViewController -> UIHostingController 서브클래싱

--- RetsTalk/RetsTalk/User/Controller/UserSettingViewController.swift
+++ RetsTalk/RetsTalk/User/Controller/UserSettingViewController.swift
@@ -8,23 +8,11 @@
 import UIKit
 import SwiftUI
 
-final class UserSettingViewController: UIViewController {
-    let userSettingView = UserSettingView()
-    
+final class UserSettingViewController: UIHostingController<UserSettingView> {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpNavigationBar()
-        setUpHostingController(userSettingView)
-    }
-    
-    private func setUpHostingController(_ hostingView: some View) {
-        let hostingController = UIHostingController(rootView: hostingView)
-        
-        addChild(hostingController)
-        hostingController.view.frame = self.view.bounds
-        self.view.addSubview(hostingController.view)
-        hostingController.didMove(toParent: self)
     }
     
     private func setUpNavigationBar() {
@@ -36,15 +24,13 @@ final class UserSettingViewController: UIViewController {
             target: self,
             action: #selector(backwardButtonTapped)
         )
-
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.leftBarButtonItem?.tintColor = .blazingOrange
         navigationItem.rightBarButtonItem?.tintColor = .blazingOrange
     }
     
-    @objc private func backwardButtonTapped() {
-        
-    }
+    @objc private func backwardButtonTapped() {}
 }
 
 // MARK: - UserSettingViewDelegate conformance
