6a93b4988199513d723e3896674088f49c5a6a45
Hanseung Cho <1win2@naver.com>
2024-11-28 11:48:08 +0900

    refactor: 라이프사이클 메서드 개선

--- RetsTalk/RetsTalk/Utility/View/BaseViewController.swift
+++ RetsTalk/RetsTalk/Utility/View/BaseViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class BaseViewController: UIViewController, UINavigationControllerDelegate {
+    
+    // MARK: UIKit lifecycle method
+    
+    override func loadView() { }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.delegate = self
+        setupNavigationBar()
+    }
+    
+    // MARK: RetsTalk lifecycle method
+    
+    func setupNavigationBar() { }
+    
+    // MARK: Navigation handling
+    
     /// 현재 네비게이션 컨트롤러 상태에 따라 뷰 컨트롤러를 pop 또는 dismiss합니다.
     func didTapNavigationBackButton() {
         if let navigationController = self.navigationController,
@@ -17,9 +39,7 @@ class BaseViewController: UIViewController, UINavigationControllerDelegate {
             self.dismiss(animated: true, completion: nil)
         }
     }
-    
-    // MARK: UINavigationControllerDelegate conformance
-    
+        
     /// 네비게이션 스택 위치를 기준으로, 스와이프 제스처로 뒤로가기 동작을 설정합니다.
     func navigationController(
         _ navigationController: UINavigationController,
