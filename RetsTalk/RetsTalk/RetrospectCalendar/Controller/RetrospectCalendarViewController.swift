7dce28d058d61e6bd9e9eb9ef13c7337d282cd79
김민석|Minseok Kim <alstjr7437@naver.com>
2024-11-30 19:08:55 +0900

    feat: UI 구성 요소 TableView, CalendarView 구성

--- RetsTalk/RetsTalk/RetrospectCalendar/Controller/RetrospectCalendarViewController.swift
+++ RetsTalk/RetsTalk/RetrospectCalendar/Controller/RetrospectCalendarViewController.swift
@@ -5,9 +5,28 @@
 //  Created by KimMinSeok on 11/30/24.
 //
 
+import Foundation
+
 final class RetrospectCalendarViewController: BaseViewController {
+    private let retrospectCalendarView: RetrospectCalendarView
+    
+    // MARK: Initalization
+    
+    init() {
+        retrospectCalendarView = RetrospectCalendarView()
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        view = retrospectCalendarView
     }
 }
--- RetsTalk/RetsTalk/RetrospectCalendar/View/RetrospectCalendarView.swift
+++ RetsTalk/RetsTalk/RetrospectCalendar/View/RetrospectCalendarView.swift
@@ -7,7 +7,55 @@
 
 import UIKit
 
-@MainActor
 final class RetrospectCalendarView: UIView {
+    private let calendarView = UICalendarView()
+    private let retrospectListTableView = UITableView()
     
+    // MARK: Initalization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .backgroundMain
+        calendarViewSetUp()
+        retrospectListTableViewSetUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: View SetUp
+    
+    private func calendarViewSetUp() {
+        addSubview(calendarView)
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            calendarView.leftAnchor.constraint(equalTo: leftAnchor),
+            calendarView.rightAnchor.constraint(equalTo: rightAnchor),
+        ])
+        
+        calendarView.wantsDateDecorations = true
+    }
+    
+    private func retrospectListTableViewSetUp() {
+        addSubview(retrospectListTableView)
+        retrospectListTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            retrospectListTableView.topAnchor.constraint(equalTo: calendarView.bottomAnchor),
+            retrospectListTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            retrospectListTableView.leftAnchor.constraint(equalTo: leftAnchor),
+            retrospectListTableView.rightAnchor.constraint(equalTo: rightAnchor),
+        ])
+        
+        retrospectListTableView.separatorStyle = .none
+        retrospectListTableView.backgroundColor = .backgroundMain
+        retrospectListTableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: Constants.retrospectCellIdentifier
+        )
+    }
 }
