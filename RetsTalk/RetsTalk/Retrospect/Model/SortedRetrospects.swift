383e12692106d99ed5244538e47481bfbd8cd163
Hanseung Cho <1win2@naver.com>
2024-12-03 03:42:49 +0900

    feat: 총회고수, 회고일수 반환을 위한 연산프로퍼티 제공

--- RetsTalk/RetsTalk/Retrospect/Model/SortedRetrospects.swift
+++ RetsTalk/RetsTalk/Retrospect/Model/SortedRetrospects.swift
@@ -5,6 +5,8 @@
 //  Created by HanSeung on 12/2/24.
 //
 
+import Foundation
+
 struct SortedRetrospects {
     private let retrospects: [[Retrospect]]
     
@@ -14,4 +16,24 @@ struct SortedRetrospects {
     
     subscript(row: Int) -> [Retrospect] { retrospects[row] }
     subscript(row: Int, column: Int) -> Retrospect { retrospects[row][column] }
+    
+    var totalCount: Int { retrospects.flatMap { $0 }.count }
+    var datesCount: Int {
+        let allRetrospects = retrospects.flatMap { $0 }
+        let uniqueDates = Set(allRetrospects.map { retrospect in
+            let formatter = DateFormatter()
+            formatter.dateFormat = Texts.dateFormat
+            return formatter.string(from: retrospect.createdAt)
+        })
+        
+        return uniqueDates.count
+    }
+}
+
+// MARK: - Constants
+
+private extension SortedRetrospects {
+    enum Texts {
+        static let dateFormat = "yyyy-MM-dd"
+    }
 }
