107dd9f3b21d1c707d4e0f5c439320c2a5e1cefe
MoonGoon <glass23000@naver.com>
2024-12-03 13:34:45 +0900

    Revert "Merge branch 'develop' into refactor/userSettingManager#177"
    This reverts commit a817d714e9b8ec9c75e22f6203df7566e79c44a8, reversing
    changes made to 99a2fa842e7a9f4f7e94b15ebf23b4cca22bcc60.

--- RetsTalk/RetsTalk/User/Model/UserSettingManager.swift
+++ RetsTalk/RetsTalk/User/Model/UserSettingManager.swift
@@ -31,7 +31,7 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
     func initialize() async -> UUID? {
         do {
             let request = PersistFetchRequest<UserData>(fetchLimit: 1)
-            let fetchedData = try userDataStorage.fetch(by: request)
+            let fetchedData = try await userDataStorage.fetch(by: request)
             guard let storedUserData = fetchedData.first
             else { return initializeUserData() }
             
@@ -47,7 +47,7 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
     func fetch() {
         let request = PersistFetchRequest<UserData>(fetchLimit: 1)
         Task {
-            let fetchedData = try userDataStorage.fetch(by: request)
+            let fetchedData = try await userDataStorage.fetch(by: request)
             guard let storedUserData = fetchedData.first else { return }
             
             await MainActor.run {
@@ -89,7 +89,7 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
     
     private func update(to updatingData: UserData) {
         Task {
-            let updatedData = try userDataStorage.update(from: updatingData, to: updatingData)
+            let updatedData = try await userDataStorage.update(from: updatingData, to: updatingData)
             await MainActor.run {
                 userData = updatedData
             }
@@ -101,7 +101,7 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
         let newNickname = randomNickname()
         let newUserData = UserData(dictionary: ["userID": newUserID.uuidString, "nickname": newNickname])
         Task {
-            let addedData = try userDataStorage.add(contentsOf: [newUserData])
+            let addedData = try await userDataStorage.add(contentsOf: [newUserData])
             guard let addedData = addedData.first else { return }
             
             await MainActor.run {
@@ -113,7 +113,7 @@ final class UserSettingManager: UserSettingManageable, @unchecked Sendable, Obse
     
     private func initiateUserData() {
         Task {
-            let addedData = try userDataStorage.add(contentsOf: [UserData(dictionary: [:])])
+            let addedData = try await userDataStorage.add(contentsOf: [UserData(dictionary: [:])])
             guard let addedData = addedData.first else { return }
             
             await MainActor.run {
