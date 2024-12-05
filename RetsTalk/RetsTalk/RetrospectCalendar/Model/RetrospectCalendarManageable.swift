69b478f6afeddf82f404e7e253a642d13539e6da
김민석|Minseok Kim <alstjr7437@naver.com>
2024-12-05 16:21:25 +0900

    feat: 회고 캘린더 매니저에 필요한 함수 추상화

--- RetsTalk/RetsTalk/RetrospectCalendar/Model/RetrospectCalendarManageable.swift
+++ RetsTalk/RetsTalk/RetrospectCalendar/Model/RetrospectCalendarManageable.swift
@@ -5,3 +5,15 @@
 //  Created by KimMinSeok on 12/5/24.
 //
 
+import Combine
+
+@RetrospectActor
+protocol RetrospectCalendarManageable: Sendable {
+    var retrospects: [Retrospect] { get }
+    var retrospectsPublisher: AnyPublisher<[Retrospect], Never> { get }
+    var errorPublisher: AnyPublisher<Error, Never> { get }
+    
+    func retrospectChatManager(of retrospect: Retrospect) -> RetrospectChatManageable?
+    func fetchRetrospects(of kindList: [Retrospect.Kind])
+    func finishRetrospect(_ retrospect: Retrospect) async
+}
